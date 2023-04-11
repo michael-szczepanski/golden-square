a425178e17e8517d7805472d05067aabce0028a7
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-04-11 16:05:45 +0100

    second test added

--- phase-1/challenge-1/spec/report_length_spec.rb
+++ phase-1/challenge-1/spec/report_length_spec.rb
@@ -5,4 +5,9 @@ describe 'report_length' do
     length = report_length("str")
     expect(length).to eq "This string was 3 characters long."
   end
+
+  it 'returns "This string was 0 characters long." when given an empty string' do
+    length = report_length("")
+    expect(length).to eq "This string was 0 characters long."
+  end
 end
