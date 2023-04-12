1b4d7b19f7a771f4cee26a23047256d862de435e
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-04-12 10:33:30 +0100

    phase1 challenge3 challenge added

--- phase-1/challenge-3/spec/password_checker_spec.rb
+++ phase-1/challenge-3/spec/password_checker_spec.rb
@@ -1,2 +1,14 @@
 require './lib/password_checker.rb'
 
+RSpec.describe PasswordChecker do
+  it 'throws "Invalid password, must be 8+ characters." error if entered "pass"' do
+    password_checker = PasswordChecker.new
+    expect { password_checker.check("pass") }.to raise_error "Invalid password, must be 8+ characters."
+  end
+
+  it 'returns true if entered "password"' do
+    password_checker = PasswordChecker.new
+    result = password_checker.check("password")
+    expect(result).to eq true
+  end
+end
