3447ddd95c3fd1991cc827e0ba8673bc0c18c60b
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-04-17 11:57:30 +0100

    phase3 challenge5 InteractiveCalculator error testing added

--- phase-3/challenge-5/interactive-calculator/lib/interactive_calculator.rb
+++ phase-3/challenge-5/interactive-calculator/lib/interactive_calculator.rb
@@ -5,10 +5,16 @@ class InteractiveCalculator
 
   def run
     @io.puts "Hello. I will subtract two numbers.\nPlease enter a number"
-    number_1 = @io.gets.chomp
+    number_1 = get_number
     @io.puts "Please enter another number"
-    number_2 = @io.gets.chomp
+    number_2 = get_number
     @io.puts "Here is your result:\n#{number_1} - #{number_2} = #{number_1.to_i - number_2.to_i}"
+  end
+
+  def get_number
+    number = @io.gets.chomp
+    fail "number needs to be a string" unless number.to_i.to_s == number
+    return number 
   end
 end
 
--- phase-3/challenge-5/interactive-calculator/spec/interactive_calculator_spec.rb
+++ phase-3/challenge-5/interactive-calculator/spec/interactive_calculator_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe InteractiveCalculator do
     interactive_calculator = InteractiveCalculator.new(io_dbl)
     interactive_calculator.run
   end
+  context 'raises "number needs to be a string" error' do
+    it 'fails' do
+      io_dbl = double :io
+      expect(io_dbl).to receive(:puts).with("Hello. I will subtract two numbers.\nPlease enter a number")
+      expect(io_dbl).to receive(:gets).and_return('four')
+      
+      interactive_calculator = InteractiveCalculator.new(io_dbl)
+      expect {
+        interactive_calculator.run
+      }.to raise_error "number needs to be a string"
+    end
+  end
 end
